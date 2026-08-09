c6493c0544e91d83d72b568c8522fd9520e66169
rafi7391 <rafi37230@gmail.com>
2026-08-09 20:55:51 +0530

    Added new files

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const {createTodo} = require("./types");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todos",function(req,res){
-
+app.post("/todo",function(req,res){
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg : "You sent the wrong inputs",
+        })
+        return;
+    }
+    // put it in mongoDB
 })
 
 app.get("/todos",function(req,res){
 
 })
 
-app.put("/todos",function(req,res){
-    
+app.put("/completed",function(req,res){
+    const updatedPayload = req.body;
+    const parsePayload = updatedPayload.safeParse(updatedPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg : "You sent the wrong inputs"
+        })
+    }
 })
